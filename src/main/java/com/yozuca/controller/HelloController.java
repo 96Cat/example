5aa098b9d3c26838cfa5f249a42acce3d9380ffc
amazarashi <eexs@foxmail.com>
2018-07-13 00:54:27 +0800

    trigger webhook

--- src/main/java/com/yozuca/controller/HelloController.java
+++ src/main/java/com/yozuca/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
 	@GetMapping("/hello")
 	public ResponseEntity<Object> hello(){
-		return ResponseEntity.ok("Hello WebHook!!!");
+		return ResponseEntity.ok("Hello WebHook");
 	}
 	
 }
